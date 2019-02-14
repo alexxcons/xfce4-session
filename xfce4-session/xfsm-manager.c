3134e90268e885d41186006ecf0f2931083a2785
Alexander Schwinn <alexxcons@xfce.org>
2019-02-14 21:38:40 +0100

    replaced deprecated "gtk_display_get_screen"

--- xfce4-session/xfsm-manager.c
+++ xfce4-session/xfsm-manager.c
@@ -603,14 +603,12 @@ xfsm_manager_load_failsafe (XfsmManager   *manager,
 {
   FailsafeClient *fclient;
   gchar          *failsafe_name;
-  GdkDisplay     *display;
   gchar           propbuf[4096];
   gchar         **command;
   gchar           command_entry[256];
   gchar           screen_entry[256];
   gint            count;
   gint            i;
-  gint            n_screen;
 
   failsafe_name = xfconf_channel_get_string (channel, "/general/FailsafeSessionName", NULL);
   if (G_UNLIKELY (!failsafe_name))
@@ -634,8 +632,6 @@ xfsm_manager_load_failsafe (XfsmManager   *manager,
       return FALSE;
     }
 
-  display = gdk_display_get_default ();
-
   g_snprintf (propbuf, sizeof (propbuf), "/sessions/%s/Count", failsafe_name);
   count = xfconf_channel_get_int (channel, propbuf, 0);
 
@@ -649,26 +645,11 @@ xfsm_manager_load_failsafe (XfsmManager   *manager,
 
       g_snprintf (screen_entry, sizeof (screen_entry),
                   "/sessions/%s/Client%d_PerScreen", failsafe_name, i);
-      if (xfconf_channel_get_bool (channel, screen_entry, FALSE))
-        {
-          for (n_screen = 0; n_screen < XScreenCount (gdk_x11_display_get_xdisplay (display)); ++n_screen)
-            {
-              fclient = g_new0 (FailsafeClient, 1);
-              if (n_screen == 0)
-                fclient->command = command;
-              else
-                fclient->command = g_strdupv (command);
-              fclient->screen = gdk_display_get_screen (display, n_screen);
-              g_queue_push_tail (manager->failsafe_clients, fclient);
-            }
-        }
-      else
-        {
-          fclient = g_new0 (FailsafeClient, 1);
-          fclient->command = command;
-          fclient->screen = gdk_screen_get_default ();
-          g_queue_push_tail (manager->failsafe_clients, fclient);
-        }
+
+      fclient = g_new0 (FailsafeClient, 1);
+      fclient->command = command;
+      fclient->screen = gdk_screen_get_default ();
+      g_queue_push_tail (manager->failsafe_clients, fclient);
     }
 
   if (g_queue_peek_head (manager->failsafe_clients) == NULL)
